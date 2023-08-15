d4701fd8ab13b9e40640dc129412b568c65eb536
Parfa Longs <longsparfa@gmail.com>
2023-08-15 13:24:07 +0100

    links functionality checked on mobile

--- app/components/Toggle.jsx
+++ app/components/Toggle.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import Link from "next/link";
+//import Link from "next/link";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 
@@ -15,17 +15,17 @@ const Toggle = () => {
         
 
         {toggle && (
-             <div className='flex justify-flex-end align-flex-end flex-col text-align bg-#031B34 p-[2rem] absolute top-[40px] right-0 mt-[1rem] min-w-[210px] rounded-[5px] shadow-[0_0_5px_rgba(0,0,0,0.2)]'>
+             <div className='flex justify-flex-end align-flex-end flex-col text-align bg-#031B34 p-[2rem] absolute top-[40px] right-0 mt-[1rem] min-w-[210px] rounded-[5px] shadow-[0_0_5px_rgba(0,0,0,0.2)] z-[99999]'>
           
-             <Link href="#projects" className='text-[2rem] leading-[32px] my-[1rem] mx-0 text-[rgba(255,255,255,0.75)] transition 0.4s ease mr-4 hover:text-white'>
-               Projects
-             </Link>
-             <Link href="#tech" className='text-[2rem] leading-[32px] my-[1rem] mx-0 text-[rgba(255,255,255,0.75)] transition 0.4s ease mr-4 hover:text-white'>
-                Technologies
-             </Link>
-             <Link href="#about" className='text-[2rem] leading-[32px] my-[1rem] mx-0 text-[rgba(255,255,255,0.75)] transition 0.4s ease mr-4 hover:text-white'>
-               About
-             </Link>
+             <a href="#projects" className='text-[2rem] leading-[32px] my-[1rem] mx-0 text-[rgba(255,255,255,0.75)] transition 0.4s ease mr-4 hover:text-white'>
+                  Project
+             </a>
+             <a href="#tech" className='text-[2rem] leading-[32px] my-[1rem] mx-0 text-[rgba(255,255,255,0.75)] transition 0.4s ease mr-4 hover:text-white'>
+                 Technologies
+             </a>
+             <a href="#about" className='text-[2rem] leading-[32px] my-[1rem] mx-0 text-[rgba(255,255,255,0.75)] transition 0.4s ease mr-4 hover:text-white'>
+                 About
+             </a>
            
          </div>
         )}
