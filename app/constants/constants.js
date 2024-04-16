70e75c2d739c2725b936cc8fc433f00d7249e293
Parfa Longs <longsparfa@gmail.com>
2024-04-16 20:52:16 +0100

    updates amazon clone

--- app/constants/constants.js
+++ app/constants/constants.js
@@ -1,12 +1,11 @@
 export const projects = [
   {
-    title: "An Image Gallery",
-    description:
-      "A fully functional and responsive image gallery that showcases a collection of images in a visually appealling way. It allwows a user to Login to the gallery page if registered. Once users are logged in they would be able to drag and drop images as well as effortlessly rearrange them within the gallery ",
-    image: "/images/P7.png",
-    tags: ["ReactJS", "Tailwindcss", "JavaScript"],
-    source: "https://github.com/Longsparfa/image-gallery",
-    visit: "https://comforting-ganache-393e38.netlify.app/",
+    title: "Frontend Amazon Clone",
+    description: "Displays available products in a visually appealling way. ",
+    image: "/images/P8.png",
+    tags: ["React/Redux", "Tailwindcss", "JavaScript"],
+    source: "https://github.com/Longsparfa/amazon-clone",
+    visit: "https://frontend-amazon-clone-sable.vercel.app/",
     id: 4,
   },
   {
@@ -51,6 +50,16 @@ export const projects = [
     source: "https://github.com/Longsparfa/budget-tracker",
     visit: "https://budget-tracker-sooty-one.vercel.app/",
     id: 3,
+  },
+  {
+    title: "An Image Gallery",
+    description:
+      "A fully functional and responsive image gallery that showcases a collection of images in a visually appealling way. It allwows a user to Login to the gallery page if registered. Once users are logged in they would be able to drag and drop images as well as effortlessly rearrange them within the gallery ",
+    image: "/images/P7.png",
+    tags: ["ReactJS", "Tailwindcss", "JavaScript"],
+    source: "https://github.com/Longsparfa/image-gallery",
+    visit: "https://comforting-ganache-393e38.netlify.app/",
+    id: 4,
   },
 ];
 
